569ccbdb73748f96be53804b1adf48ae8916e96c
Seongyun <seongyun0326@gmail.com>
2022-11-22 15:42:08 +0900

    Initial Commit

---
+++ MVVMDemo/MVVMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMDemoApp.swift
+//  MVVMDemo
+//
+//  Created by Seongyun Kim on 2022/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
